fca96c04ab02a10a22a07433851ce0b620701d2b
Lei Wang <tendant@gmail.com>
2013-03-26 20:47:39 -0700

    add exiting.

--- src/wackmole/core.clj
+++ src/wackmole/core.clj
@@ -1,6 +1,6 @@
 (ns wackmole.core)
 
-
+(def score (atom 0))
 
 (defn pick []
   (nth ["X" "O"] (rand-int 2)))
@@ -11,7 +11,7 @@
 (defn int-val [x]
   (mod (dec (Integer/valueOf x)) 3))
 
-(defn foo
+(defn play
   "I don't do a whole lot."
   []
   (let [expected (pickgrid)]
@@ -19,10 +19,44 @@
     (println "Where to wack?")
     (let [_ (println "Pick X:")
           x (int-val (read-line))
+          _ (println (inc x))
           _ (println "Pick Y:")
           y (int-val (read-line))
+          _ (println (inc y))
           z (+ (* 3 y) x)
           ]
       (= "X" (nth expected z))
       ))
   )
+
+(defn score-result [result]
+  (if result
+    (swap! score inc)
+    (swap! score dec)
+    )
+  (println "Your current score is: " @score)
+  (println)
+  )
+
+(defn message [result]
+  (if result
+    "You hit the mole!"
+    "Just missed"))
+    
+  
+(defn check-result [result]
+  (do
+    (println)
+    (println (message result))
+    (score-result result)
+    ))
+
+(defn multi-play []
+   (try
+     (check-result
+      (play))
+     (multi-play)
+     (catch java.lang.NumberFormatException e
+       (println "Exiting nicely...")
+       )
+     ))
