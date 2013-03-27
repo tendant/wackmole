e4b7f6a6bba43205fabc601c4f492fd2b6ba70a4
Lei Wang <tendant@gmail.com>
2013-03-26 20:15:47 -0700

    step 1

--- src/wackmole/core.clj
+++ src/wackmole/core.clj
@@ -1,14 +1,19 @@
 (ns wackmole.core)
 
+
+
 (defn pick []
   (nth ["X" "O"] (rand-int 2)))
 
+(defn pickgrid []
+  (assoc (vec (repeat 9 "O")) (rand-int 9) "X"))
 
 (defn foo
   "I don't do a whole lot."
   []
-  (let [expected [(pick) (pick) (pick)]]
-    (println expected)
+  (let [expected (pickgrid)]
+    (doall (map println (partition 3 expected)))
+    (println "Where to wack?")
     (let [input (read-line)
           aa (dec (Integer/valueOf input))
           ]
